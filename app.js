3e8cd3b9c2d0b8b0a338e4dcbc5694dd3295dc1a
EC2 Default User <ec2-user@ip-172-31-6-48.us-west-2.compute.internal>
2017-04-29 20:47:06 +0000

    db exists

--- app.js
+++ app.js
@@ -5,6 +5,38 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+/* database modules */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost:27017/StuFree')
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', 
+	function() {
+		console.log("Successfully connected to MongoDB with Mongoose");
+});
+
+var user_schema = new Schema({
+	name: String,
+	email: String,
+	password: String,
+	rating: Number,
+	location: String,
+	description: String
+})
+
+var job_schema = new Schema({
+	title: String,
+	description: String,
+	icon: String,
+	time: String,
+	price: Number
+})
+var User = mongoose.model('User', user_schema);
+var Job = mongoose.model('Job', job_schema);
+
+/* Routes */
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -24,6 +56,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+
+app.post('/create_job', function(req, res) {
+
+}
+
+app.post('/login', function(req, res) {
+
+});
+
+app.post('/register', function(req, res) {
+
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
